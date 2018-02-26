bd061778bbf65491f3cf825ea79ad71186c0c719
Sam Bruno <sjbrunster@gmail.com>
2018-02-26 16:13:23 -0500

    add subtitle to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,9 @@ const Header = () => (
   <div className="header">
     <div className="container">
       <h1 className="header__title">Misquoted</h1>
+      <h3 className="header__subtitle">
+        Recast the roles of classic motion pictures
+      </h3>
     </div>
   </div>
 );
