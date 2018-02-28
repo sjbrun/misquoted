db8c3cd6aed9a3837af9e7a4fe5f8f3acd1341e0
Sam Bruno <sjbrunster@gmail.com>
2018-02-28 18:44:14 -0500

    remove unnecessary fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,44 @@
 const path = require('path');
 
-console.log(path.join(__dirname, 'public'))
+module.exports = (env) => {
+    const isProduction = env === 'production';
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    node: {
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
+    return {
+        entry: './src/index.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        node: {
+            fs: 'empty',
+            net: 'empty',
+            tls: 'empty'
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }, {
+                test: /\.(ttf|eot|woff|woff2)$/,
+                loader: 'file-loader',
+                exclude: /node_modules/,
+                options: {
+                  name: '[path]][name].[ext]'
+                },
+              },
             ]
-        }, {
-            test: /\.(ttf|eot|woff|woff2)$/,
-            loader: 'file-loader',
-            exclude: /node_modules/,
-            options: {
-              name: '[path]][name].[ext]'
-            },
-          },
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
-    }
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public')
+        }
+    };    
 };
