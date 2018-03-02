6e1fd29b566b7d24dd1defe5335f8902b344ff4b
Sam Bruno <sjbrunster@gmail.com>
2018-03-02 16:11:15 -0500

    add styling for mobile screens

--- src/components/CardHeader.js
+++ src/components/CardHeader.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 const CardHeader = (props) => (
   <div className="card__header">
-    <h3 className="impression__title">{props.impression}</h3>
-    <h4>in</h4>
-    <h3 className="movie__title">{props.movie}</h3>
+    <div>
+      <h3 className="impression__title">{props.impression}</h3>
+    </div>
+    <div>   
+      <h4 className="in">in</h4>
+    </div>
+    <div>
+      <h3 className="movie__title">{props.movie}</h3>
+    </div>
   </div>
 );
 
