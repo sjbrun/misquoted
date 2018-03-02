1c7ace35399c766ab7aa81d8807349165a45c109
Sam Bruno <sjbrunster@gmail.com>
2018-03-02 14:42:15 -0500

    setup dev source maps, extract text webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,59 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
-    const isProduction = env === 'production';
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
-    return {
-        entry: './src/index.js',
-        output: {
-            path: path.join(__dirname, 'public'),
-            filename: 'bundle.js'
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    node: {
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty'
+    },
+    module: {
+      rules: [{
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      }, {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        loader: 'file-loader',
+        exclude: /node_modules/,
+        options: {
+          name: '[path]][name].[ext]'
         },
-        node: {
-            fs: 'empty',
-            net: 'empty',
-            tls: 'empty'
-        },
-        module: {
-            rules: [{
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }, {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }, {
-                test: /\.(ttf|eot|woff|woff2)$/,
-                loader: 'file-loader',
-                exclude: /node_modules/,
-                options: {
-                  name: '[path]][name].[ext]'
-                },
-              },
-            ]
-        },
-        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
-        devServer: {
-            contentBase: path.join(__dirname, 'public')
-        }
-    };    
+      }]
+    },
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public')
+    }
+  };
 };
